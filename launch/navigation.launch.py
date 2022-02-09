11ce97c4401aee78995e3681269285d14c0e398f
coalman321 <cjtucker321@gmail.com>
2022-02-08 21:11:17 -0500

    sorta working nav

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -1,20 +1,40 @@
 import launch
 import launch.actions
 from ament_index_python.packages import get_package_share_directory
-import launch_ros.actions
+from launch.substitutions import LaunchConfiguration as LC
 import os
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import TextSubstitution, LaunchConfiguration
-from launch_ros.actions import PushRosNamespace
+import xacro
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction
+from launch_ros.actions import Node
+
+# evaluates LaunchConfigurations in context for use with xacro.process_file(). Returns a list of launch actions to be included in launch description
+def evaluate_xacro(context, *args, **kwargs):
+    robot = LC('namespace').perform(context)
+    debug = LC('debug').perform(context)
+
+    modelPath = launch.substitutions.PathJoinSubstitution([
+        get_package_share_directory('riptide_descriptions2'),
+        'robots',
+        robot + '.xacro'
+    ]).perform(context)
+
+    xacroData = xacro.process_file(modelPath,  mappings={'debug': debug, 'namespace': robot, 'inertial_reference_frame':'world'}).toxml()
+
+    robot_state_publisher = Node(
+        name = 'robot_state_publisher',
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output = 'screen',
+        parameters=[
+            {'robot_description': xacroData},
+            {'use_tf_static': True}
+            ], # Use subst here
+    )
+
+
+    return [robot_state_publisher]
 
 def generate_launch_description():
-
-    # Read in the vehicle's namespace through the command line or use the default value one is not provided
-    launch.actions.DeclareLaunchArgument(
-        "namespace", 
-        default_value="puddles",
-        description="Namespace of the vehicle",
-    )
 
     # declare the launch args to read for this file
     config = os.path.join(
@@ -23,9 +43,22 @@ def generate_launch_description():
         'ekf_config.yaml'
     )
 
-    return launch.LaunchDescription([        
+    return launch.LaunchDescription([ 
+        # Read in the vehicle's namespace through the command line or use the default value one is not provided
+        DeclareLaunchArgument(
+            "namespace", 
+            default_value="puddles",
+            description="Namespace of the vehicle",
+        ),
+
+        DeclareLaunchArgument(
+            "debug", 
+            default_value="False",
+            description="enable xacro debug of the vehicle",
+        ),
+
         # start robot_localization Extended Kalman filter (EKF)
-        launch_ros.actions.Node(
+        Node(
             package='robot_localization',
             executable='ekf_node',
             # type='ekf_localization_node',
@@ -38,19 +71,19 @@ def generate_launch_description():
                 'reset_on_time_jump': True,
             }
             ]),
-        launch_ros.actions.Node(
+        Node(
             package='riptide_localization2',
             executable='dvl_converter',
             name='dvl_converter',
         ),
-        launch_ros.actions.Node(
+        Node(
             package='riptide_localization2',
             executable='depth_converter',
             name='depth_converter',
         ),
 
         # A joint state publisher
-        launch_ros.actions.Node(
+        Node(
             name="joint_state_publisher",
             package="joint_state_publisher",
             executable="joint_state_publisher",
@@ -58,18 +91,11 @@ def generate_launch_description():
         ),
 
         # Publish robot model for Sensor locations
-        launch_ros.actions.Node(
-            name="robot_state_publisher",
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            respawn=True,
-            output="screen",
-            parameters=[{'use_tf_static': True}],
-        ),
+        OpaqueFunction(function=evaluate_xacro),
 
         # Publish world and odom as same thing until we get SLAM
         # This is here so we can compare ground truth from sim to odom
-        launch_ros.actions.Node(
+        Node(
             name="odom_to_world_broadcaster",
             package="tf2_ros",
             executable="static_transform_publisher",
